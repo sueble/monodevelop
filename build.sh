09ba4b5c34c4f6721b5940069f024bbb4e7d8b80
Joseph Stroman <joewstroman@gmail.com>
2017-08-08 16:55:08 -0400

    Don't change directory name during checkout

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 echo "PATH ======= $PATH"
 echo "WORKSPACE ======= $WORKSPACE"
 
-cd .. && mv s monodevelop && cd monodevelop
-
-if [ "$1" = "checkout" ]; then 
-    rake -f $WORKSPACE/wrench/monodevelop/Rakefile checkout 
+if [ "$1" = "checkout" ]; then
+	cd ..
+	git clone git@github.com:xamarin/release-scripts
+    rake -f release-scripts/wrench/monodevelop/Rakefile checkout
 else
+	cd .. && mv s monodevelop && cd monodevelop
     make $1
+    cd .. && mv monodevelop s && cd s
 fi;
-
-cd .. && mv monodevelop s && cd s
